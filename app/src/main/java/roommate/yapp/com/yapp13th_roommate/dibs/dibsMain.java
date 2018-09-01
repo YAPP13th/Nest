7ca172183f6f7950e50b7f94c7657edac19da465
Sung-Jin <osj4872@gmail.com>
2018-09-01 23:11:09 +0900

    select layout update 1

---
+++ app/src/main/java/roommate/yapp/com/yapp13th_roommate/dibs/dibsMain.java
@@ -0,0 +1,16 @@
+package roommate.yapp.com.yapp13th_roommate.dibs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import roommate.yapp.com.yapp13th_roommate.R;
+
+public class dibsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
